127739c59d6e0a97a59e5131cccda57fd35e9998
Pegas83 <88933872+Pegas83@users.noreply.github.com>
2021-12-14 15:21:58 +0300

    Add files via upload

--- Tensor_test.py
+++ Tensor_test.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-import pytest
 from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome()
@@ -12,13 +11,8 @@ search_field.send_keys("Тензор")
 
 
 def test_search_field():
-    # search_field = browser.find_element(By.XPATH, '//*[@id="text"]')
     assert search_field, "There is no search field"
-    # search_field.send_keys("Тензор")
 
-
-# search_field = browser.find_element(By.XPATH, '//*[@id="text"]')
-# search_field.send_keys("Тензор")
 
 def test_suggest():
     suggest = browser.find_element(By.XPATH, '/html/body/div[3]')
@@ -40,17 +34,3 @@ def test_first_5_links_to_tensor():
     tensor_number = link_list.count('tensor.ru')
     assert tensor_number == 5, f'There are no first five links leading to tensor.ru. Only {tensor_number} of them'
 
-# time.sleep(3)
-# browser.close()
-# //*[@id="search-result"]/li[2]/div/div[1]/div[1]/a/b
-# //*[@id="search-result"]/li[1]/div/div[1]/div[1]/a/b
-# //*[@id="search-result"]/li[1]/div/div[1]/div[1]
-
-# Path Organic-Path Organic-Path_verified path organic__path
-# //*[@id="search-result"]/li[1]/div/div[1]/div[1]/div[1]
-#
-# Path Organic-Path path organic__path
-# //*[@id="search-result"]/li[2]/div/div[1]/div[1]
-#
-# Path Organic-Path path organic__path
-# //*[@id="search-result"]/li[3]/div/div[1]/div[1]
